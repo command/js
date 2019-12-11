dd6144708cb12fb34c0c1be1b28321952ea7cf92
Ryan Glover <business@cleverbeagle.com>
2019-12-10 18:25:32 -0600

    wip

--- index.js
+++ index.js
@@ -1,2 +1,53 @@
-// Write your package code here.
 "use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _axios = require("axios");
+
+var _axios2 = _interopRequireDefault(_axios);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Command = function () {
+  function Command(apiKey, options) {
+    _classCallCheck(this, Command);
+
+    if (!apiKey) this.throwFormattedError("");
+    this._validateOptions();
+  }
+
+  _createClass(Command, [{
+    key: "throwFormattedError",
+    value: function throwFormattedError(error) {
+      throw new Error("[Command] " + error + " See https://docs.oncommand.io/api/libraries#javascript for detailed usage instructions.");
+    }
+  }, {
+    key: "_validateBehaviorKey",
+    value: function _validateBehaviorKey(behaviorKey) {
+      var requiredFormatRegex = new RegExp(/[^_\W]+:[^_\W]+/g);
+      var matchedRequiredFormat = requiredFormatRegex.test(behaviorKey);
+
+      if (!matchedRequiredFormat) {
+        this.throwFormattedError(behaviorKey + " does not match the required format featureKey:behaviorKey.");
+      }
+    }
+  }, {
+    key: "track",
+    value: function track(behaviorKey) {
+      // command.track();
+      // command.customers.create()
+
+      this._validateBehaviorKey(behaviorKey);
+    }
+  }]);
+
+  return Command;
+}();
+
+exports.default = Command;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,62 @@
-// Write your package code here.
+import axios from "axios";
+
+class Command {
+  constructor(apiKey, options) {
+    if (!apiKey) this.throwFormattedError("");
+    this._validateOptions(options);
+  }
+
+  _validateBehaviorKey(behaviorKey) {
+    const requiredFormatRegex = new RegExp(/[^_\W]+:[^_\W]+/g);
+    const matchedRequiredFormat = requiredFormatRegex.test(behaviorKey);
+
+    if (!matchedRequiredFormat) {
+      this.throwFormattedError(
+        `${behaviorKey} does not match the required format featureKey:behaviorKey.`
+      );
+    }
+  }
+
+  _validateTrackProperties(properties) {
+    if (!properties) return;
+    if (properties && typeof properties !== "object") {
+      this.throwFormattedError(
+        "Properties argument passed to command.track must be an object."
+      );
+    }
+  }
+
+  _throwFormattedError(error) {
+    throw new Error(
+      `[Command] ${error} See https://docs.oncommand.io/api/libraries#javascript for detailed usage instructions.`
+    );
+  }
+
+  _request() {
+    axios({
+      method: "post",
+      url: "http://localhost:4000/api/v1"
+    });
+  }
+
+  track(behaviorKey, properties) {
+    this._validateBehaviorKey(behaviorKey);
+    this._validateTrackProperties(properties);
+  }
+
+  _createCustomer() {
+    this.request("");
+  }
+
+  _updateCustomer() {}
+
+  _deleteCustomer() {}
+
+  static customers = {
+    create: this._createCustomer,
+    update: this._updateCustomer,
+    delete: this._updateCustomer
+  };
+}
+
+export default Command;
