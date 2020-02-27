6c34b27113a4f45b72be0b02abc91fba72e6bd4f
Ryan Glover <business@cleverbeagle.com>
2020-02-27 14:55:03 -0600

    release v0.8.0

--- release.js
+++ release.js
@@ -88,6 +88,7 @@ const commitReleaseToRepo = async version => {
   try {
     await promiseExec(`git add . && git commit -m "release v${version}"`);
   } catch (exception) {
+    console.warn(exception);
     throw new Error(`[release.commitReleaseToRepo] ${exception.message}`);
   }
 };
