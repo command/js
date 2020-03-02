119b758454d3ad46d6de66faaebd35d715083718
Ryan Glover <business@cleverbeagle.com>
2020-03-02 09:04:52 -0600

    add log so we can easily debug requests

--- src/index.js
+++ src/index.js
@@ -24,6 +24,14 @@ class CommandAPI {
 
   _request(method, path, data = {}, callback = null) {
     // NOTE: http://localhost:4000/api is dynamically swapped to https://api.oncommand.io in /release.js when releasing a new version. Leave as-is for local dev.
+    console.log({
+      method,
+      url: `http://localhost:4000/api/v1${path}`,
+      headers: {
+        "x-api-key": this.apiKey
+      },
+      data
+    });
     return axios({
       method,
       url: `http://localhost:4000/api/v1${path}`,
